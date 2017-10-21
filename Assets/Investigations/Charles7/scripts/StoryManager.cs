4d38bdff0b2d1ef0011a3afe023cd6c6357b7cd3
GregoireMilliez <gregoire@milliez.fr>
2017-10-21 23:04:37 +0200

    [File] reorganize project + adding story in xml

---
+++ Assets/Investigations/Charles7/scripts/StoryManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Load XML
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Manage which chapter should be display
+	}
+}
---
+++ Assets/Investigations/Tests/scripts/TestLocationService.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TestLocationService : MonoBehaviour
+{
+
+    public Text testText;
+    private float count = 0;
+
+    private float gpsAccuracy = 1.0f;
+    private float gpsUpdateInterval = 1.0f;
+
+    IEnumerator Start()
+    {
+        Input.compass.enabled = true;
+        Input.location.Start(gpsAccuracy, gpsUpdateInterval);
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+            yield break;
+
+        
+
+        
+    }
+
+    void Update()
+    {
+        // Start service before querying location
+        
+        Debug.Log("Latitude : " + Input.location.lastData.latitude);
+        Debug.Log("Timestamp : " + Input.location.lastData.timestamp);
+        testText.text = "Latitude: " + Input.location.lastData.latitude + " Longitude: " + Input.location.lastData.longitude + " Altitude: " + Input.location.lastData.altitude + " HorizontalAccuracy: " + Input.location.lastData.horizontalAccuracy + " Timestamp: " + Input.location.lastData.timestamp;
+        count++;
+    }
+
+
+
+}
